e6aa4046f730dc31f9a36c52123281f2e99a78b1
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-11-15 17:38:41 +0200

    register & login

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 // Connect To the Database
-const { Client } = require("pg");
-const client = new Client({
+const { Pool } = require("pg");
+const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -10,21 +10,26 @@ const client = new Client({
   port: process.env.DB_PORT,
 });
 
-(function initiateDbTables() {
-  client.connect();
-  client
-    .query(
-      `CREATE TABLE IF NOT EXISTS users(
-      email VARCHAR ( 255 ) UNIQUE NOT NULL,
-      password VARCHAR ( 255 ) NOT NULL
-      )`
-    )
-    .then((res) => {
-      console.log(res);
-      client.end();
-    })
-    .catch((e) => {
-      console.error(e);
-      client.end();
-    });
-})();
+const findUserByEmail = async (email) => {
+  const user = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+    email,
+  ]);
+  return user.rows[0];
+};
+
+const createUser = async (email, password) => {
+  try {
+    const user = await pool.query(
+      `INSERT INTO users (email, password) VALUES ('${email}', '${password}') RETURNING *`
+    );
+    return user.rows[0];
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = {
+  pool,
+  createUser,
+  findUserByEmail,
+};
--- index.js
+++ index.js
@@ -1,10 +1,69 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-require("./db");
+app.use(bodyParser.json());
+const { pool, createUser, findUserByEmail } = require("./db");
+
+(function initiateDbTables() {
+  pool
+    .query(
+      `CREATE TABLE IF NOT EXISTS users(
+      email VARCHAR ( 255 ) UNIQUE NOT NULL,
+      password VARCHAR ( 255 ) NOT NULL
+      )`
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+})();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  createUser("test", "hd");
+});
+
+app.post("/register", async (req, res) => {
+  console.log("body", req.body);
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).send("Email is required");
+  }
+  if (!password) {
+    return res.status(400).send("Password is required");
+  }
+
+  const user = await findUserByEmail(email);
+  if (user) {
+    return res.status(400).send("User already exists");
+  }
+
+  const newUser = await createUser(email, password);
+  res.send(newUser);
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).send("Email is required");
+  }
+  if (!password) {
+    return res.status(400).send("Password is required");
+  }
+
+  const user = await findUserByEmail(email);
+  if (!user) {
+    return res.status(400).send("User does not exist");
+  }
+
+  if (user.password !== password) {
+    return res.status(400).send("Password is incorrect");
+  }
+
+  res.send(user);
 });
 
 app.listen(3000, () => {
